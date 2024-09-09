b2231e3471ba619ef3a98b477af8debde4f702b5
Emil Lykke Diget <emil@lykke-diget.dk>
2024-09-09 11:15:56 +0200

    Added estimator parent class.

---
+++ include/sdu_estimators/estimators/estimator.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#ifndef ESTIMATOR_HPP
+#define ESTIMATOR_HPP
+
+namespace sdu_estimators::estimators
+{
+
+/**
+ * This class provides a base class for the different estimators for estimating the parameter in the linear regression
+ * equation (LRE) defined as:
+ *
+ * \f$ y(t) = \phi^T(t) \theta(t), \f$
+ *
+ * where \f$ y : \mathbb{R}_+ \to \mathbb{R}^n \f$ is the output, \f$ \phi : \mathbb{R}_+ \to \mathbb{R}^{m \times n} \f$ is,
+ * the regressor matrix and \f$ \theta : \mathbb{R}_+ \to \mathbb{R}^m \f$ is the parameter vector.
+ */
+
+class Estimator
+{
+  /**
+   * @brief Step the execution of the estimator (must be called in a loop externally)
+   */
+  virtual void step();
+
+  /**
+   * @brief Get the current estimate of the parameter. Updates when the step function is called.
+   */
+  virtual void get_estimate();
+
+  /**
+   * @brief Reset internal estimator variables
+   */
+  virtual void reset();
+
+  virtual ~Estimator() = default;
+}
+
+} //namespace sdu_estimators::estimators
+
+#endif //ESTIMATOR_HPP
