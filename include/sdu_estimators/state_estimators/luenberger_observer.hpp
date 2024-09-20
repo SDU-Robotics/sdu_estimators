7b92171bae77af0482810934d6c8ab27e74f0440
Emil Lykke Diget <eld@mmmi.sdu.dk>
2024-09-20 08:26:02 +0200

    The Luenberger observer is now templated. State space model as member instead of parent -- inheritance is tricky.

--- include/sdu_estimators/state_estimators/luenberger_observer.hpp
+++ include/sdu_estimators/state_estimators/luenberger_observer.hpp
@@ -5,7 +5,7 @@
 namespace sdu_estimators::state_estimators
 {
   template <typename T, int32_t DIM_Nx, int32_t DIM_Nu, int32_t DIM_Ny>
-  class LuenbergerObserver : public StateSpaceModel<T, DIM_Nx, DIM_Nu + DIM_Ny, DIM_Ny>
+  class LuenbergerObserver // : public StateSpaceModel<T, DIM_Nx, DIM_Nu + DIM_Ny, DIM_Ny>
   {
   public:
     /**
@@ -34,11 +34,16 @@ namespace sdu_estimators::state_estimators
       newD = newB;
       newD.setZero();
 
-      this->StateSpaceModel<T, DIM_Nx, DIM_Nu + DIM_Ny, DIM_Ny>::~StateSpaceModel();                 // destroy the base class
-      new (this) StateSpaceModel<T, DIM_Nx, DIM_Nu + DIM_Ny, DIM_Ny>(newA, newB, C, newD, Ts, method);  // overwrites the base class storage with a new instance
-      // new (this) StateSpaceModel(newA, newB, C, newD);  // overwrites the base class storage with a new instance
-      // std::cout << "Bd\n" << this->getBd() << std::endl;
-      StateSpaceModel<T, DIM_Nx, DIM_Nu + DIM_Ny, DIM_Ny>::reset();
+      // this->StateSpaceModel<T, DIM_Nx, DIM_Nu + DIM_Ny, DIM_Ny>::~StateSpaceModel();                 // destroy the base class
+      // this->StateSpaceModel::~StateSpaceModel();                 // destroy the base class
+      // // new (this) StateSpaceModel<T, DIM_Nx, DIM_Nu + DIM_Ny, DIM_Ny>(newA, newB, C, newD, Ts, method);  // overwrites the base class storage with a new instance
+      // new (this) StateSpaceModel(newA, newB, C, newD, Ts, method);
+      // // std::cout << "Bd\n" << this->getBd() << std::endl;
+      // // StateSpaceModel<T, DIM_Nx, DIM_Nu + DIM_Ny, DIM_Ny>::reset();
+      // StateSpaceModel::reset();
+
+      // sys(newA, newB, C, newD, Ts, method);
+      sys = new StateSpaceModel<T, DIM_Nx, DIM_Nu + DIM_Ny, DIM_Ny>(newA, newB, C, newD, Ts, method);
     }
 
     void update(Eigen::Vector<T, DIM_Ny> & y, Eigen::Vector<T, DIM_Nu> & u)
@@ -46,13 +51,28 @@ namespace sdu_estimators::state_estimators
       Eigen::Vector<T, DIM_Nu + DIM_Ny> uu;
       uu << u, y;
 
-      StateSpaceModel<T, DIM_Nx, DIM_Nu + DIM_Ny, DIM_Ny>::update(uu);
+      // StateSpaceModel<T, DIM_Nx, DIM_Nu + DIM_Ny, DIM_Ny>::update(uu);
+      sys->update(uu);
     }
 
     Eigen::Vector<T, DIM_Nx> get_state_estimate()
     {
-      return StateSpaceModel<T, DIM_Nx, DIM_Nu + DIM_Ny, DIM_Ny>::get_state();
+      // return StateSpaceModel<T, DIM_Nx, DIM_Nu + DIM_Ny, DIM_Ny>::get_state();
+      return sys->get_state();
     }
+
+    Eigen::Matrix<T, DIM_Nx, DIM_Nx> getAd()
+    {
+      return sys->getAd();
+    }
+
+    Eigen::Matrix<T, DIM_Nx, DIM_Nu + DIM_Ny> getBd()
+    {
+      return sys->getBd();
+    }
+
+  private:
+    StateSpaceModel<T, DIM_Nx, DIM_Nu + DIM_Ny, DIM_Ny> * sys;
   };
 }
 
