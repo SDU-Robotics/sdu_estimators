00daac3e881fc8b9574095c1570f2225b7d5369e
Emil Lykke Diget <eld@mmmi.sdu.dk>
2024-12-04 08:46:42 +0100

    Skeleton manifold.

--- include/sdu_estimators/math/riemannian_manifolds/manifold.hpp
+++ include/sdu_estimators/math/riemannian_manifolds/manifold.hpp
@@ -15,6 +15,8 @@ namespace sdu_estimators::math::manifold
   * - Metric
   * - Distance
   * - Exponential
+  * - Logarithmic
+  * - Retraction
   * - Euclidean gradient -> Riemannian gradient
   * - Projection
   */
@@ -24,16 +26,53 @@ namespace sdu_estimators::math::manifold
   public:
     virtual ~Manifold() = default;
 
+   /**
+    * The geodesic distance between two points on the manifold
+    * @param point_a
+    * @param point_b
+    * @return distance
+    */
+    virtual T dist(point &point_a, point &point_b) = 0;
+
     /**
-     * The geodesic distance between two points on the manifold
+     * Projects vector to tangent space.
+     * @param point_
+     * @param vector_
+     * @return A vector in the tangent space at the given point.
+     */
+    virtual vector projection(point &point_, vector &vector_) = 0;
+
+    /**
+     * Converts from Euclidean gradient to Riemannian gradient.
+     * @param point_
+     * @param euclidean_gradient
+     * @return Riemannian gradient.
+     */
+    virtual vector euclidean_to_riemannian_gradient(point &point_, vector &euclidean_gradient) = 0;
+
+    /**
+     * Retraction from tangent space to the manifold.
+     * @param point_
+     * @param trangent_vector
+     * @return
+     */
+    virtual point retraction(point &point_, vector &trangent_vector) = 0;
+
+    /**
+     * Exponential map on the manifold.
+     * @param point_
+     * @param tangent_vector
+     * @return
+     */
+    virtual point exp(point &point_, vector &tangent_vector) = 0;
+
+    /**
+     * Logarithmic map on the manifold.
      * @param point_a
      * @param point_b
      * @return
      */
-    virtual T dist(point &point_a, point &point_b) = 0;
-
-  
-    virtual vector projection(point &point_, vector &vector_) = 0;
+    virtual vector log(point &point_a, point &point_b) = 0;
   };
 }
 
